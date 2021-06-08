aeabf8faad8e0b006aeef6879262798ba3af5186
jlrichards2121 <65782493+jlrichards2121@users.noreply.github.com>
2021-06-08 16:03:02 -0700

    My ui.R and server.R files

---
+++ server.R
@@ -0,0 +1,29 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        x    <<- rnorm(input$numb_samp)
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = input$radio, border = 'white', 
+             main = paste("Histogram of Normal Distribution with ", 
+                          input$numb_samp, " samples and ",
+                          input$bins, " bins"))
+    })
+    
+
+})
